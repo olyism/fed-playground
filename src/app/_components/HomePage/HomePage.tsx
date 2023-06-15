ec7175fe8006d4318ded3dfe005df05e5b660f5e
Oly Su <taiya1229@gmail.com>
2023-06-15 10:36:48 +1000

    💄 Add link from homepage to other pages

--- src/app/_components/HomePage/HomePage.tsx
+++ src/app/_components/HomePage/HomePage.tsx
@@ -1,7 +1,17 @@
 import type { FC } from 'react'
+import Link from 'next/link'
+import Container, { Variant } from '@/components/Container'
 
 const HomePage: FC = () => (
-  <main>Homepage</main>
+  <section className="min-h-screen flex items-center">
+    <Container variant={Variant.Inner}>
+      <nav className="text-center">
+        <ul>
+          <li><Link href="/radio">Radio page</Link></li>
+        </ul>
+      </nav>
+    </Container>
+  </section>
 )
 
 export default HomePage
