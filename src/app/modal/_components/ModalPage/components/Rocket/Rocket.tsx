4a7de84438e8e54bcdb7fde857130180f26adeee
Oly Su <taiya1229@gmail.com>
2023-07-05 14:38:01 +1000

    💄 White-out modal background

--- src/app/modal/_components/ModalPage/components/Rocket/Rocket.tsx
+++ src/app/modal/_components/ModalPage/components/Rocket/Rocket.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react'
 import styles from './Rocket.module.css'
 
 const Rocket: FC = () => (
-  <video className={styles.rocket} width="600" height="100%" autoPlay loop muted playsInline>
+  <video className={styles.rocket} width="100%" height="100%" autoPlay muted playsInline>
     <source src="rocket.webm" type="video/webm" />
     <source src="rocket.mp4" type='video/mp4; codecs="hvc1"' />
   </video>
